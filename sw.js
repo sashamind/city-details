a9bddc51e0168d4ff397d99c0e7f62303656dff7
AlexanderMindrin <mindrin.alexander@gmail.com>
2026-02-12 21:21:35 +0300

    откат к рабочей версии

--- sw.js
+++ sw.js
@@ -1,1 +1,34 @@
-self.addEventListener("install", function(e) { self.skipWaiting(); }); self.addEventListener("activate", function(e) { e.waitUntil(caches.keys().then(function(names) { return Promise.all(names.map(function(name) { return caches.delete(name); })); })); }); self.addEventListener("fetch", function(e) {});
+const CACHE_NAME = 'city-details-v2';
+const urlsToCache = [
+    '/city-details/',
+    '/city-details/index.html',
+    '/city-details/src/style.css',
+    '/city-details/src/main.js',
+    '/city-details/manifest.json',
+    '/city-details/icon-192.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((names) =>
+            Promise.all(
+                names.filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            )
+        )
+    );
+});
